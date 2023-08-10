eddb89a128cd4903ec7e7faeeda0ab3f6ac68dbd
Hope Avalon Gagodo <hopegag@gmail.com>
2023-08-10 14:49:39 +0000

    Update pythonserverset.sh

--- pythonserverset.sh
+++ pythonserverset.sh
@@ -2,7 +2,11 @@
 sudo apt install build-essential zlib1g-dev \
 libncurses5-dev libgdbm-dev libnss3-dev \
 libssl-dev libreadline-dev libffi-dev curl software-properties-common
+sudo apt-get install libmysqlclient-dev
+libmariadb-dev-compat libmariadb-dev
+#Pyscog fix
+sudo apt install libpq-dev python3-dev
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install libgl1-mesa-glx
-sudo apt-get install python3.9 python3.9-dev python3.9-venv
+sudo apt-get install python3.10 python3.10-dev python3.10-venv
 sudo apt-get install libmysqlclient-dev libmariadb-dev-compat libmariadb-dev
