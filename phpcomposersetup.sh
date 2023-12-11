77484a373a903f9bac0c11082d5dd904d2d3d43d
Hope Avalon Gagodo <hopegag@gmail.com>
2023-12-11 23:22:58 +0000

    Update phpcomposersetup.sh

--- phpcomposersetup.sh
+++ phpcomposersetup.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
-sudo apt update && sudo apt -y upgrade
+sudo wget https://nginx.org/keys/nginx_signing.key
+sudo apt-key add nginx_signing.key
+
+echo "###" | sudo tee -a /etc/apt/sources.list
+echo "deb https://nginx.org/packages/mainline/ubuntu/ bionic nginx" | sudo tee -a /etc/apt/sources.list
+echo "deb-src https://nginx.org/packages/mainline/ubuntu/ bionic nginx" | sudo tee -a /etc/apt/sources.list
+
+sudo apt-get remove nginx-common
+sudo apt-get update
+
+sudo apt-get install curl
+
+echo "deb http://security.ubuntu.com/ubuntu focal-security main" | sudo tee /etc/apt/sources.list.d/focal-security.list
+sudo apt-get update
+sudo apt-get install libssl1.1
+
+sudo apt-get install nginx
+
+sudo rm /etc/apt/sources.list.d/focal-security.list
+
 sudo apt update
 sudo apt install lsb-release ca-certificates apt-transport-https software-properties-common -y
 sudo add-apt-repository ppa:ondrej/php
-sudo apt install php8.1 -y
+sudo apt install php8.2 -y
 
-sudo apt install php8.1-{bcmath,xml,fpm,mysql,zip,intl,ldap,gd,cli,bz2,curl,mbstring,pgsql,opcache,soap,cgi} -y
-
+sudo apt install php8.2-{bcmath,xml,fpm,mysql,zip,intl,ldap,gd,cli,bz2,curl,mbstring,pgsql,opcache,soap,cgi} -y
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+
 php composer-setup.php
-php -r "unlink('composer-setup.php');"
 
 sudo mv composer.phar /usr/local/bin/composer
+
+php -r "unlink('composer-setup.php');"
